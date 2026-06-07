94fe5cf99afa71a88ba0eac0b8dceae8cd10c4d3
AshFoxware <fantauzzoboyz@gmail.com>
2026-06-08 08:58:53 +1000

    20260608_AF_ Refactored for mobile!

---
+++ server.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml'
+};
+
+const server = http.createServer(async (req, res) => {
+    // Default route
+    let filePath = req.url === '/'
+        ? './public/index.html'
+        : './public' + req.url;
+
+    const extname = path.extname(filePath);
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (err, content) => {
+
+        if (err) {
+
+            if (err.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+
+        } else {
+
+            res.writeHead(200, {
+                'Content-Type': contentType
+            });
+
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
